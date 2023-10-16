a6b5ba08091d757575bec4626552c4ec140a2de6
32priyadharshini <priyadharshini121998@gmail.com>
2023-10-16 14:55:36 +0530

    my commit

--- src/main/java/com/example/demo/mywebappController.java
+++ src/main/java/com/example/demo/mywebappController.java
@@ -4,8 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class mywebappController {
-	@GetMapping("/get")
+public class MywebappController {
+	@GetMapping(value="/get")
 	public String get() {
 		return "this is program";
 	}
